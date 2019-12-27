4f9bab8c3b2793aded0ad6ea4ddbab5575d0d937
AlexGarcia <afgarcia666@gmail.com>
2019-12-27 12:40:20 -0500

    created character model js file

---
+++ db/model/characters.js
@@ -0,0 +1,17 @@
+// import connection file
+const mongoose = require('../connection');
+// now create schema varible for mongoose
+const Schema = mongoose.Schema
+// create model for character data 
+const Character = new Schema({
+    char_id: Number,
+    name: String,
+    birthday: String,
+    occupation: [String],
+    img: String,
+    status: String,
+    nickname: String,
+    portrayed: String
+})
+
+module.exports = mongoose.model('Character', Country)
