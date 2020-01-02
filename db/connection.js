300253edf7c43c52c083ac11394aeaf2a7d7fa2c
AlexGarcia <afgarcia666@gmail.com>
2020-01-02 15:32:13 -0500

    fixed connection code

--- db/connection.js
+++ db/connection.js
@@ -8,9 +8,11 @@ if (process.env.NODE_ENV === "production") {
     mongoURI = "mongodb://localhost/breakingBad_db"
 }
 
-mongoose.connect("mongodb://localhost/breakingBad_db",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(instance =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch(error => console.log("Connection failed!", error));
 
 module.exports = mongoose;
