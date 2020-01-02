63f1ef089ba19e9969c082b0b27b544791977f88
AlexGarcia <afgarcia666@gmail.com>
2020-01-02 12:55:33 -0500

    routed character file

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,6 @@
-const data = require('./data.json')
+const data = require('./data.json');
 
-const Character = require('./model/Characters')
+const Character = require('./model/Characters');
 
 Character.remove({})
     .then(() => {
